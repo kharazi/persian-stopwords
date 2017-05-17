f41308b4e902b58d39211d059146c95e6ce6994b
Vahid Kharazi <vahid@kharazi.net>
2017-05-17 15:05:50 +0430

    Remove some words

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import codecs
 from sets import Set
+from hazm import Normalizer
 
-persian = "\n".join(list(Set(open('persian').read().split('\n'))))
-p_file = open('persian', 'w')
-p_file.write(persian)
+
+nmz = Normalizer()
+stops = "\n".join(
+    sorted(
+        list(
+            Set(
+                [
+                    nmz.normalize(w) for w in codecs.open('persian', encoding='utf-8').read().split('\n') if w
+                ]
+            )
+        )
+    )
+)
+print stops
+
+codecs.open('out', 'w', encoding='utf-8').write(stops)
